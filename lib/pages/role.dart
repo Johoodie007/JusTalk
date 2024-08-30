bc935a900c0eb831ef1e7daf6e24450aa94d6b48
Johoodie007 <yaah1221@gmail.com>
2024-08-30 13:54:27 +0300

    JJ3

--- lib/pages/role.dart
+++ lib/pages/role.dart
@@ -1,14 +1,84 @@
 import 'package:flutter/material.dart';
-//import 'package:flutter_svg/flutter_svg.dart';
-//import 'package:google_fonts/google_fonts.dart';
 
 class Role extends StatelessWidget {
   const Role({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Role Page'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Choose Your Role'),
+        backgroundColor: Colors.deepPurple,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Please select your role:',
+              style: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+                color: Colors.deepPurple,
+              ),
+            ),
+            const SizedBox(height: 40),
+            RoleOption(
+              role: 'Psychotherapist',
+              icon: Icons.medical_services,
+              onTap: () {
+                Navigator.pushNamed(context, '/psychotherapist');
+              },
+            ),
+            const SizedBox(height: 20),
+            RoleOption(
+              role: 'Patient',
+              icon: Icons.person,
+              onTap: () {
+                Navigator.pushNamed(context, '/patient');
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class RoleOption extends StatelessWidget {
+  final String role;
+  final IconData icon;
+  final VoidCallback onTap;
+
+  const RoleOption(
+      {super.key, required this.role, required this.icon, required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: Colors.deepPurple[50],
+        ),
+        child: Row(
+          children: <Widget>[
+            Icon(icon, size: 30, color: Colors.deepPurple),
+            const SizedBox(width: 20),
+            Text(
+              role,
+              style: const TextStyle(
+                fontSize: 20,
+                color: Colors.deepPurple,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
