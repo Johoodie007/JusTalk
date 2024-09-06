408d5dac7cb0127c68dabda055577a31881a11da
Johoodie007 <yaah1221@gmail.com>
2024-09-06 16:40:13 +0300

    JJ5

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-//import 'package:flutter_app/pages/doc.dart';
+// import 'package:flutter_app/pages/doc.dart';
 //import 'package:flutter_app/pages/get_started.dart';
 //import 'package:flutter_app/pages/splash.dart';
 
